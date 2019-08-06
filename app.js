bc86a258eab991a1690da68019d02dc937c03aa6
RAJ VIKRAM SINGH <38528859+Raj-Vikram-Singh@users.noreply.github.com>
2019-08-06 10:16:04 +0530

    Basic Functionality covered

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const notFav =  "fa-heart-o";
 let favClass;
 
 let jsonData = [];
+let favoriteList=[];
+
 
 function getDataService(){
 
@@ -43,17 +45,22 @@ function renderView(viewArray){
 
 
 
-function markAsFavorite(elementId){
+function markAsFavorite(element){
     
          for(i=0; i< jsonData.length; i++)
-         if( jsonData[i].id == elementId){
+         if( jsonData[i].id == element.parentNode.id){
 
-             jsonData[i].favorite = true;
+             jsonData[i].favorite = jsonData[i].favorite ? false :true;            
 
-             document.getElementById(elementId).querySelector(".heart").classList.toggle(fav);
-             document.getElementById(elementId).querySelector(".heart").classList.toggle(notFav);
+            //  document.getElementById(elementId).querySelector(".heart").classList.toggle(fav);
+            //  document.getElementById(elementId).querySelector(".heart").classList.toggle(notFav);
+            element.classList.toggle(fav);
+            element.classList.toggle(notFav);
              
-             break;
+             setLocalStorage(jsonData);
+
+             return;
+            //  break;
            
          }
    
@@ -65,13 +72,19 @@ content.addEventListener('click', function(event){
     let element = event.target;
     let elementId;
     if(element.title ==="heart"){
-        elementId= element.parentNode.id;
-        markAsFavorite(elementId);
+      //  elementId= element.parentNode.id;
+        markAsFavorite(element);
     }  
 });
 
 
  function onLoad(){
+     let localStoredData = getLocalStorage();
+     if(localStoredData){
+         jsonData = localStoredData;
+         renderView(jsonData);
+         return;
+     }
      getDataService().then(data => {
          renderView(data);
      } );
@@ -79,4 +92,87 @@ content.addEventListener('click', function(event){
 
  onLoad();
 
+ function searchHotel(input){
+
+    let filteredList=[];
+
+   
+    filteredList = jsonData.filter(item => item.name.toUpperCase().includes(input.toUpperCase()) );
+    document.querySelectorAll('.restaurant').forEach(function(el) {
+        el.style.display = 'none';
+     });
+
+     filteredList.forEach(item =>{
+        document.getElementById(item.id).style.display = "block";
+     });
+
+ }
+
+ function cuisineFilter(input){
+    let filteredList=[];
+    filteredList = jsonData.filter(item => item.tags.includes(input) );
+    document.querySelectorAll('.restaurant').forEach(function(el) {
+        el.style.display = 'none';
+     });
+
+     filteredList.forEach(item =>{
+        document.getElementById(item.id).style.display = "block";
+     });
+
+ }
+
+ function sort(input){
+    
+    
+
+    if(document.getElementById("filterMessage")){
+        document.getElementById("filterMessage").style.display = "none"; 
+    }
+
+    document.querySelectorAll('.restaurant').forEach(function(el) {
+        el.style.display = 'none';
+     });
+
+     if(input === 'rating'){
+
+        jsonData = jsonData.sort((a,b) =>{
+           return b.rating - a.rating;
+        });
+
+        renderView(jsonData);
+     }
+
+     else if(input === 'ETA'){
+        jsonData = jsonData.sort((a,b) =>{
+            return a.ETA - b.ETA;
+         });
+
+         renderView(jsonData);
+
+     }
+
+    else if(input === 'favorite'){
+        favoriteList = jsonData.filter(item => item.favorite === true);
+
+         if(!favoriteList[0]){
+            content.insertAdjacentHTML("beforeend", "<div id = 'filterMessage'> There are no Favorites Restaurants saved</div>");
+            return;
+         }
+
+         renderView(favoriteList);
+     }    
+ }
+
+ function setLocalStorage(storeData){
+
+    localStorage.setItem('hotelList', JSON.stringify(storeData));
+ }
+
+ function getLocalStorage(){
+    return JSON.parse(localStorage.getItem('hotelList'));
+ }
  
+ function refresh(){
+    localStorage.clear();
+    location.reload();
+}
